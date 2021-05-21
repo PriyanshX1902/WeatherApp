878b4cd6153c5ccae3478f18c1fb1086c6e253a0
PriyanshX1902 <priyansh.bhatnagar@rediff.com>
2021-05-21 22:22:23 +0530

    Created basic layout

---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(MaterialApp(
+      title: 'Weather-app',
+      home: Home(),
+    ));
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  var temp;
+  var humidity;
+  var currently;
+  var description;
+  var wind;
+
+  Future getWeather() async {
+    http.Response response = await http.get(Uri.parse(
+        "http://api.openweathermap.org/data/2.5/weather?q=delhi&units=metric&appid=3aa9f0e85efd351d791bea6e1622b42c"));
+    var results = jsonDecode(response.body);
+    setState(() {
+      this.temp = results['main']['temp'];
+      this.description = results['weather'][0]['description'];
+      this.currently = results['weather'][0]['main'];
+      this.wind = results['wind']['speed'];
+      this.humidity = results['main']['humidity'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getWeather();
+  }
+
+  @override
+  Widget build(BuildContext buildContext) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.red,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(top: 40.0, bottom: 10.0),
+                    child: Text("Currently in Delhi",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w600,
+                        ))),
+                Text(
+                  temp != null ? temp.toString() + "\u00B0" : "Loading",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: Text(
+                        currently != null ? currently.toString() : "Loading",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w600,
+                        )))
+              ],
+            ),
+          ),
+          Expanded(
+              child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                  title: Text("Temperature"),
+                  trailing: Text(
+                      temp != null ? temp.toString() + "\u00B0" : "Loading"),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.cloud),
+                  title: Text("Weather"),
+                  trailing: Text(
+                      description != null ? description.toString() : "Loading"),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.sun),
+                  title: Text("Humidity"),
+                  trailing:
+                      Text(humidity != null ? humidity.toString() : "Loading"),
+                ),
+                ListTile(
+                  leading: FaIcon(FontAwesomeIcons.wind),
+                  title: Text("Wind Speed"),
+                  trailing: Text(
+                      wind != null ? wind.toString() + "km/hr" : "Loading"),
+                )
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
